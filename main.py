fc74474d0ac3a1db04deacdb09860cb6ac0ec615
szachMati <mateusz.hawryluk30@gmail.com>
2022-05-27 18:42:19 +0200

    add simulated annealing algorithm

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 
 import random
-from math import exp, cos, pi, sqrt
+from math import cos, pi, sqrt
 
 from numpy import asarray
 from numpy import exp
-from numpy.random import rand
 
 from scipy.constants import Boltzmann
 
@@ -20,27 +19,32 @@ def ackley(xVector):
 
 
 def generate_start_point(bounds):
-	return bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+	""" funkcja generująca losowy punkt startowy z podanego zakresu
+		:parameter bounds - wektor temperatur
+	"""
+	minimum = min(bounds[0])
+	maximum = max(bounds[0])
+	return [random.uniform(minimum, maximum)]
 
 def mutate(xVector):
 	array = []
 	for i in range(len(xVector)):
-		array.append(xVector[i] + random.gauss(1, 10))
+		array.append(xVector[i] + random.gauss(0, 1))
 	return array
 
-def simulatedAnnealing(bounds, cost_fn, n, start_temp=350, stop_temp=15, cool=0.95):
+def simulatedAnnealing(tempArray, cost_fn, n, start_temp=100, stop_temp=15, cool=0.95):
 	""" simulated annealing algorithm
-	 	:parameter bounds - given temperatures
-	 	:parameter cost_fm - cost function which we minimize
-	 	:parameter start_temp - starting temperature
-	 	:parameter stop_temp - stop temperatur
-	 	:parameter cool - cooling
+	 	:parameter tempArray - wektor temperatur
+	 	:parameter cost_fm - funkcja kosztu, którą minimalizujemy
+	 	:parameter start_temp - początkowa temperatura
+	 	:parameter stop_temp - temperatura końcowa
+	 	:parameter cool - wartość chłodzenia
 	 """
 	k = Boltzmann
-	print(f"range{bounds}")
-	x = generate_start_point(bounds)
+	print(f"zakres od {min(tempArray[0])} do {max(tempArray[0])}")
+	x = generate_start_point(tempArray)
 	xValue = cost_fn(x)
-	print(f"start point x: {x}, xValue: {xValue}")
+	print(f"Pkt początkowy x: {x}, xValue: {xValue}")
 	temperature = start_temp
 	while temperature > stop_temp:
 		for i in range(0, n):
@@ -55,8 +59,9 @@ def simulatedAnnealing(bounds, cost_fn, n, start_temp=350, stop_temp=15, cool=0.
 					x = xPrim
 					xValue = xPrimValue
 		temperature = temperature * cool
+		print(f"x: {x}, xValue: {xValue}, temp: {temperature}")
 	return x, xValue, temperature
 
 if __name__ == '__main__':
-	result = simulatedAnnealing(asarray([[-100, 100]]), ackley, 1000)
-	print(f"result x: {result[0]}, f(x): {result[1]}, temp: {result[2]}")
+	result = simulatedAnnealing(asarray([[-100, 100]]), ackley, 100)
+	print(f"Wynik x: {result[0][0]}, f(x): {result[1]}, temp: {result[2]}")
